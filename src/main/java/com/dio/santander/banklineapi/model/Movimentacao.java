1ba89b62f0653edc8e4ce8f7bbd97d257e280bcf
lionelsu <lionel_suicune@hotmail.com>
2022-05-03 20:56:58 -0400

    mapping units

--- src/main/java/com/dio/santander/banklineapi/model/Movimentacao.java
+++ src/main/java/com/dio/santander/banklineapi/model/Movimentacao.java
@@ -1,5 +1,44 @@
 package com.dio.santander.banklineapi.model;
 
+import java.time.LocalDateTime;
+
 public class Movimentacao {
+	private Integer id;
+	private LocalDateTime dataHora;
+	private String descicao;
+	private Double valor;
+	private MovimentacaoTipo tipo;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public LocalDateTime getDataHora() {
+		return dataHora;
+	}
+	public void setDataHora(LocalDateTime dataHora) {
+		this.dataHora = dataHora;
+	}
+	public String getDescicao() {
+		return descicao;
+	}
+	public void setDescicao(String descicao) {
+		this.descicao = descicao;
+	}
+	public Double getValor() {
+		return valor;
+	}
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	public MovimentacaoTipo getTipo() {
+		return tipo;
+	}
+	public void setTipo(MovimentacaoTipo tipo) {
+		this.tipo = tipo;
+	}
+	
+	
 
 }
